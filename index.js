7d68dd4acc01c2a86afef4edcbba1a1de39f9b1b
masterexxe <134110442+masterexxe@users.noreply.github.com>
2023-05-20 22:07:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -34,16 +34,22 @@ function getInput() {
   let finalRegex = "";
   startRegex = regexStart(inputValuesSplitAccent[0]);
 
+  console.log(rest)
 
 
+  let len = rest.length;
 
-  rest.forEach(element => {
-    if(rest !== element) {
-      finalRegexArr.push(regex(element));
-    } else {
-      endRegex = regexEnd(element);
+  for (let i = 0; i < len; i++) {
+    console.log(rest[i]);
+    if (i+1 >= len || rest[i] != rest[i+1]) {
+      console.log('oops!'); // print oops only if we're at the end of the array OR elements are different
+      finalRegexArr.push(regex(rest[i]));
     }
-  })
+  }
+
+
+  //end-part of the regex
+  finalRegexArr.push('+\\b');
 
   finalRegex = finalRegexArr.join('');
 
@@ -51,7 +57,6 @@ function getInput() {
   finalRegex = startRegex + finalRegex + endRegex;
 
 
-  //finalRegex = accents(finalRegexArr);
   stringForRegex.innerHTML = inputValue;
   finalText.innerHTML = finalRegex;
 
@@ -77,19 +82,15 @@ function regexStart(x) {
 
 }
 
-function regexEnd(x) {
-  return '+([' + x + '][\\W_]*?)+\\b';
-}
 
 function accents(s) {
 
   let r = s.toString().toLowerCase();
 
-  r = r.replace("\u00f6", "\u00f6\u006f\u00f4\u00f2\u00f3\u00f5\u0153\u00f8\u014d\u0030");
   r = r.replace("\u00df", "\u00df\u0073");
   r = r.replace("a", "\u0061\u00e4\u0034");
   r = r.replace("b", "\u0062\u0036\u0431");
-  r = r.replace("c", "\u0063\u0441\u00e7\u0107\u010d");
+  r = r.replace("c", "c");
   r = r.replace("e", "\u0065\u0033");
   r = r.replace("g", "g69");
   r = r.replace("i", "i1");
@@ -102,6 +103,5 @@ function accents(s) {
 
 
   return r.toLowerCase();
-
 
 }
